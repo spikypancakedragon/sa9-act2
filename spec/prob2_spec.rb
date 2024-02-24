94e4b76d148d243947c88b3be1f5e557fd924c93
Avery Walker <spikypancakedragon@gmail.com>
2024-02-24 17:06:00 -0600

    finished prob2

--- spec/prob2_spec.rb
+++ spec/prob2_spec.rb
@@ -1,20 +1,29 @@
+require "prob2"
+
 RSpec.describe TodoList do
+
+  before(:each) do
+    @some_list = TodoList.new
+    @some_list.add("item 1")
+  end
 
   describe "#add" do
     it "adds a todo to the list" do
-      # Fill in the test
+      @some_list.add("item 2")
+      expect(@some_list.todos).to eq(["item 1", "item 2"])
     end
   end
 
   describe "#remove" do
     it "removes a todo from the list" do
-      # Fill in the test
+      @some_list.remove("item 2")
+      expect(@some_list.todos).to eq(["item 1"])
     end
   end
 
   describe "#todos" do
     it "returns all todos" do
-      # Fill in the test
+      expect(@some_list.todos).to eq(["item 1"])
     end
   end
 
