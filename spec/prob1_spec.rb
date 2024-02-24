31373e0c6e052d5de6fa01283b9d9be0e7692218
Avery Walker <spikypancakedragon@gmail.com>
2024-02-24 16:55:02 -0600

    fixed some stuff in prob1 and started on prob2

--- spec/prob1_spec.rb
+++ spec/prob1_spec.rb
@@ -2,24 +2,28 @@ require "prob1_code"
 
 RSpec.describe User do
 
+  before(:each) do
+    @some_user = User.new("spiky")
+  end
+
   describe "#log_in" do
     it "logs the user in" do
-      some_user = User.new("spiky")
-      some_user.log_in
-      expect(some_user.logged_in).to eq(true)
+      @some_user.log_in
+      expect(@some_user.logged_in).to eq(true)
     end
   end
+
   describe "#log_out" do
     it "logs the user out" do
-      some_user = User.new("spiky")
-      some_user.log_out
-      expect(some_user.logged_in).to eq(false)
+      @some_user.log_out
+      expect(@some_user.logged_in).to eq(false)
     end
   end
+
   describe "#username" do
     it "returns the correct username" do
-      some_user = User.new("spiky")
-      expect(some_user.username).to eq("spiky")
+      expect(@some_user.username).to eq("spiky")
     end
   end
+
 end
